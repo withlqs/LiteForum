dea4e99e2811e9fbbfc9ff5064ee33ce32a612b8
zentorwie <zentorwie@gmail.com>
2015-11-19 00:42:49 +0800

    add utils

--- topic/utils.py
+++ topic/utils.py
@@ -1,1 +1,9 @@
-def user_topic_history()
+from topic.models import Topic, Reply
+
+
+def user_topic_history(username):
+    return Topic.objects.filter(author__username=username).order_by('-pub_date')
+
+
+def user_reply_history(username):
+    return Reply.objects.filter(author__username=username).order_by('-pub_date')
