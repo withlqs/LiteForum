f686dd22b514063c45a2b8a12d7c2f4863e13a51
withlqs <withlqs@gmail.com>
2015-11-19 23:05:18 +0800

    add profile

--- user/models.py
+++ user/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(null=False, max_length=30, unique=True)
-    email = models.EmailField(null=False, unique=True)
-    site = models.TextField(default='')
-    location = models.TextField(default='')
-    comment = models.TextField(default='')
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(blank=False, max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    site = models.TextField()
+    location = models.TextField()
+    comment = models.TextField()
+
+    def get_user(self):
+        user = {
+            'username': str(self.username),
+            'id': str(self.id)
+        }
+        return user
--- user/views.py
+++ user/views.py
@@ -6,6 +6,8 @@ from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import RequestContext
 from django.shortcuts import render_to_response
 
+from user.models import User
+
 # Create your views here.
 
 admin = 'administrator'
@@ -38,10 +40,19 @@ def login(request):
         if request.user.get_username() == admin:
             return HttpResponseRedirect('/manage/')
         return HttpResponseRedirect('/')
-    return views.login(request)
+    return views.login(request, template_name='accounts/login.html')
 
 
 def profile(request):
+    if request.user.is_authenticated():
+        print("$$" + request.user.get_username())
+        user = User.objects.get(username=request.user.get_username())
+        print(user.get_user()['username'])
+        return render_to_response('accounts/profile.html', {'user': user.get_user()})
+    return HttpResponseRedirect('/accounts/login/')
+
+
+def profile_edit(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('/')
     return HttpResponseRedirect('/accounts/login/')
@@ -54,11 +65,17 @@ def register(request):
         form = forms.UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            _username = form.cleaned_data['username']
+            _email = request.POST['email']
+            User(
+                username=_username,
+                email=_email
+            ).save()
             auth.login(request, user=authenticate(username=form.cleaned_data['username'],
                                                   password=form.cleaned_data['password1']))
-            return HttpResponseRedirect('/manage/')
+            return HttpResponseRedirect('/accounts/profile')
     else:
         form = auth.forms.UserCreationForm()
-    return render_to_response('registration/register.html', {
+    return render_to_response('accounts/register.html', {
         'form': form,
     }, context_instance=RequestContext(request))
