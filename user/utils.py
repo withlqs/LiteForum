44d2cb3bd1c1ec0f0af6c1e538164ad4d21e9ac2
zentorwie <zentorwie@gmail.com>
2015-11-20 06:33:01 +0800

    adjusting UI

--- user/utils.py
+++ user/utils.py
@@ -3,7 +3,8 @@ import hashlib
 
 def get_gravatar(email):
     default = "http://www.example.com/default.jpg"
-    size = 100
+    size = 60
     email_str = email.encode('utf-8')
-    gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email_str.lower()).hexdigest() + "?d=" + str(size)
+    gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email_str.lower()).hexdigest() + ".jpg?d=" + str(
+        size)
     return str(gravatar_url)
